cfe500d4c05851df6f203c1f05d0552fed0c3ade
Viktoria Papp <lilianapriest33@gmail.com>
2020-06-10 14:30:13 +0200

    print usage ready

--- ToDoApp/src/ToDoMain.java
+++ ToDoApp/src/ToDoMain.java
@@ -1,2 +1,16 @@
 public class ToDoMain {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Command Line Todo Application\n" +
+                    "=============================\n" +
+                    "\n" +
+                    "Command line arguments:\n" +
+                    "-l   Lists all the tasks\n" +
+                    "-a   Adds a new task\n" +
+                    "-r   Removes a task\n" +
+                    "-c   Completes a task");
+        }
+    }
 }
